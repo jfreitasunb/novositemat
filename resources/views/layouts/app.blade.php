b5df00dd1470d2bf812e9d9364886767cf8b9836
José Antônio Freitas <jfreitas@unb.br>
2021-04-20 16:17:36 -0300

    Página inicial do site.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,54 +5,59 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Departamento de Matemática</title>
-        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('imagens/logos/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ URL::asset('imagens/logos/favicon-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset('imagens/logos/favicon-16x16.png') }}">
         <link rel="manifest" href="{{ URL::asset('imagens/logos/site.webmanifest') }}">
 
     </head>
-    <body class="antialiased">
-        <div class="">
-            {{-- header --}}
-            <div class="">
-                {{-- left side --}}
-                <div class="">
-                    <img src="{{ asset('imagens/logos/logo.png') }}" width="140" alt="Logo">
-                </div>
-
-                {{-- right side --}}
-
-                <div class="">
-                    <h1 class="">Departamento de Matemática</h1>
-                </div>
-                <div class="">
-                      <div>
-                          <select class="">
-                            <option>Portugues</option>
-                            <option>English</option>
-                          </select>
-                      </div>
-                      <div>
-                        <form class="flex">
-                            <input class="" placeholder="Procurar"/>
-                            <button class="">Buscar</button>
-                        </form>     
-                      </div>
-                </div>
-    ´
-            </div>
-
-            {{-- Language area --}}
-
+    <body>
+        <div class="flex flex-col h-screen justify-between">
             
-            {{-- main area --}}
+            <header>
+                <div class="p-4 flex lg:justify-between md:justify-between items-center">
+                    {{-- left side --}}
+                    <div class="p-4 flex lg:justify-between md:justify-between items-centerlg:w-1/3 md:w-1/2">
+                        <a href="#"><img src="{{ asset('imagens/logos/logo.png') }}" width="140" alt="Logo"></a>
+                    </div>
 
-            {{-- footer --}}
+                    {{-- center --}}
 
-            <footer class="">
-                <p class="">Departamento de Matemática - {{ date("Y") }}</p>
-            </footer>
-        </div>
+                    <div class="lg:w-2/3 lg:self-center md:w-1/2 ml-2 lg:mr-96">
+                        <p class="text-azul-MAT lg:text-4xl md:text-4xl md:text-center lg:mb-4">Departamento de Matemática</p>
+                    </div>
+
+                    {{-- right side --}}
+                    <div class="w-1/4 align-middle">
+                          <div class="m-auto">
+                              <select class="w-3/4 py-2">
+                                <option>Portugues</option>
+                                <option>English</option>
+                              </select>
+                          </div>
+                          <div>
+                            <form class="py-4">
+                                <input class="w-1/2" placeholder="Procurar"/>
+                                <button class="w-1/4 bg-blue-400">Buscar</button>
+                            </form>     
+                          </div>
+                    </div>
+                </div>
+                
+                <div class="">
+                    <hr class="blueMAT" style="height: 4px;opacity: 1;">    
+                </div>
+            </header>
+        <main>
+        </main>
+        
+        {{-- footer --}}
+        <footer class="h-10">
+            <hr class="blueMAT" style="height: 4px;opacity: 1;">
+            <p class="">Departamento de Matemática - {{ date("Y") }}</p>
+        </footer>
+    </div>
+
     </body>
 </html>
