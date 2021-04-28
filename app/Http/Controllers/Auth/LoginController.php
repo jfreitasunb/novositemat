dfc26382beeda8856d5e97bd4da060eb81136647
José Antônio Freitas <jfreitas@unb.br>
2021-04-28 17:19:50 -0300

    Classe personalizada para login.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -19,7 +23,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    // use AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
@@ -38,11 +42,6 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
     
-    public function username()
-    {
-        return 'usuario';
-    }
-
     public function index()
     {
         return view('auth.login');
@@ -50,6 +49,76 @@ class LoginController extends Controller
 
     public function logar(Request $request)
     {
-        dd($request);
+        $credentials['usuario'] = $request->username;
+
+        $credentials['password'] = $request->password;
+
+        $this->ensureIsNotRateLimited($credentials['usuario']);
+
+        if (! Auth::attempt($credentials, $request->filled('remember'))) {
+            RateLimiter::hit($this->throttleKey($credentials['usuario']));
+
+            throw ValidationException::withMessages([
+                'username' => __('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($this->throttleKey($credentials['usuario']));
+
+        return back()->withErrors([
+            'username' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    /**
+     * Ensure the login request is not rate limited.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function ensureIsNotRateLimited($usuario)
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey($usuario), 5)) {
+            return;
+        }
+
+        event(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey($usuario));
+
+        throw ValidationException::withMessages([
+            'username' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+    /**
+     * Get the rate limiting throttle key for the request.
+     *
+     * @return string
+     */
+    public function throttleKey($username)
+    {
+        return Str::lower($username).'|'.\Request::ip();
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
