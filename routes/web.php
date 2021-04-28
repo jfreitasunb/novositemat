fc84f33f8e558da3a4b149fbd5c946b7ca52fd32
José Antônio Freitas <jfreitas@unb.br>
2021-04-28 18:12:23 -0300

    Rotas para recuperação de senha.

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,32 +21,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/gc', function (){
-    return "você pode acessar.";
-})->middleware('admin');
-
-Route::get('/login', [LoginController::class, 'index'])
-->name('login');
-
-Route::post('/login', [LoginController::class, 'logar']);
-
-Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->middleware('guest')
-                ->name('password.request');
-
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->middleware('guest')
-                ->name('password.email');
-
-Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->middleware('guest')
-                ->name('password.reset');
-
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
-                ->middleware('guest')
-                ->name('password.update');
-
-Route::post('/logout', [LoginController::class, 'logout'])
-                ->middleware('auth')
-                ->name('logout');
-
+require __DIR__.'/auth.php';
