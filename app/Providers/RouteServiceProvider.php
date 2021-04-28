6611d83348cb151235c1d1124909cd8a0c6a863a
José Antônio Freitas <jfreitas@unb.br>
2021-04-28 17:20:27 -0300

    Rota para o gerenciador de conteúdo.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/gc';
 
     /**
      * The controller namespace for the application.
