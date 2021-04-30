a6151affe2fadf9d11965ffd8f4051fa011ebfa2
José Antônio Freitas <jfreitas@unb.br>
2021-04-30 19:11:48 -0300

    Ajustes no nome da classe.

--- app/View/Components/MaisNoticias.php
+++ app/View/Components/MaisNoticias.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class MaisPrincipal extends Component
+class MaisNoticias extends Component
 {
     /**
      * Create a new component instance.
