ecf83d643f41f983d9e6a26262f413f40e9ced28
José Antônio Freitas <jfreitas@unb.br>
2021-04-30 15:26:57 -0300

    Link para a página principal.

--- resources/views/admin/gc.blade.php
+++ resources/views/admin/gc.blade.php
@@ -19,7 +19,7 @@
             <div class="p-4 flex md:justify-between items-center">
                 <!-- left side -->
                 <div class="p-4 flex md:justify-between items-center md:w-1/2">
-                    <a href="#"><img src="{{ URL::asset('imagens/logos/logo.png') }}" width="140" alt="Logo"></a>
+                    <a href="{{URL::to('/')}}"><img src="{{ URL::asset('imagens/logos/logo.png') }}" width="140" alt="Logo"></a>
                 </div>
 
                 <!-- center -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
                 <div class="p-4 flex md:justify-between items-center">
                     <!-- left side -->
                     <div class="p-4 flex md:justify-between items-center md:w-1/2">
-                        <a href="#"><img src="imagens/logos/logo.png" width="140" alt="Logo"></a>
+                        <a href="{{URL::to('/')}}"><img src="imagens/logos/logo.png" width="140" alt="Logo"></a>
                     </div>
     
                     <!-- center -->
