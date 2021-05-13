7ef0f3e1d17d6360a2d9af39501aeb5f4f80736f
José Antônio Freitas <jfreitas@unb.br>
2021-05-13 14:33:20 -0300

    Exibindo validação.

--- resources/views/admin/menus_site.blade.php
+++ resources/views/admin/menus_site.blade.php
@@ -4,13 +4,21 @@
     <div class="bg-white flex py-8 justify-center items-center">
         <div class="bg-white shadow-xl rounded p-5">
             <x-auth-errors-validation/>
+            <x-auth-status-session class="mb-4" :status="session('status')" />
             <h1 class="text-3xl font-medium">Cadastrar entrada dos menus do site</h1>
         
-            <form class="space-y-5 mt-5" method="POST" action="{{ route('menu.superior') }}">
+            <form class="space-y-5 mt-5" method="POST" action="{{ route('menus.site') }}">
                 @csrf
-                <input type="text" name="texto_pt" class="w-full h-12 border border-gray-800 rounded px-3" placeholder="Texto em Português" required/>
-                <input type="text" name="texto_en" class="w-full h-12 border border-gray-800 rounded px-3" placeholder="Texto em Inglês" required/>
-
+                <div class="mt-4 bg-gray-200">
+                    <span class="text-gray-700">Nome do Menu</span>
+                    <div class="mt-2 ml-2">
+                        <input type="text" name="texto_pt" class="w-full h-12 border border-gray-800 rounded px-3 @error('texto_pt') border-red-500 @enderror"" placeholder="Texto em Português" value="{{ old('texto_pt') }}" required/>
+                    </div>
+                    
+                    <div class="mt-2 ml-2">
+                        <input type="text" name="texto_en" class="w-full h-12 border border-gray-800 rounded px-3 @error('texto_en') border-red-500 @enderror" placeholder="Texto em Inglês" value="{{ old('texto_en') }}" required/>
+                    </div>
+                </div>
                 <div class="mt-4 bg-gray-200">
                     <span class="text-gray-700">Posição do Menu</span>
                     <div class="mt-2 ml-2">
