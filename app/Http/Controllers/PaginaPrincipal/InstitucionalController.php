80ad44d3a605da72f1f4562390e259e1d79a57e5
José Antônio Freitas <jfreitas@unb.br>
2021-05-26 15:14:24 -0300

    Controllers para algumas seções do site.

---
+++ app/Http/Controllers/PaginaPrincipal/InstitucionalController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\PaginaPrincipal;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+use Session;
+
+class InstitucionalController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {   
+        return view('layouts.app');
+    }
+}
