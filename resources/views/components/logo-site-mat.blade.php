d01a60475a74b296ad29055af1a32de9cd48cbd8
José Antônio Freitas <jfreitas@unb.br>
2021-05-26 15:14:09 -0300

    Nada importante.

--- resources/views/components/logo-site-mat.blade.php
+++ resources/views/components/logo-site-mat.blade.php
@@ -1,3 +1,3 @@
 <div class="p-4 flex md:justify-between items-center md:w-1/2">
-    <a href="{{URL::to('/')}}"><img src="{{ URL::asset('imagens/logos/logo.png') }}" width="140" alt="Logo"></a>
+    <a href="{{ URL::to('/') }}"><img src="{{ URL::asset('imagens/logos/logo.png') }}" width="140" alt="Logo"></a>
 </div>
