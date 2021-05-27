e0e80e7b8864e95552ae7f8a7698fa82b9ca2c84
José Antônio Freitas <jfreitas@unb.br>
2021-05-27 14:18:35 -0300

    Área com notícias.

---
+++ resources/views/templates/partials/mostrar_noticias.blade.php
@@ -0,0 +1,73 @@
+<div>
+    <!-- destaque à esquerda -->
+    <div class="flex inline-block justify-between items-center">
+        <div class="pl-6">
+            <span class="text-xm">{{ __('News') }}</span>
+        </div>
+
+        <hr class="style-one">
+            
+        <div class="pr-6">
+            <a class="text-xs" href="#">{{ __('See all news') }}</a>
+        </div>
+    </div>
+
+    {{-- notícia de destaque --}}
+    <div class="grid grid-cols-2">
+        <div class="p-4">
+            <img src="imagens/mat-entrada.jpg" style="width: 580px; height: 397px">
+        </div>
+
+        <!-- mais alguns destaques -->
+        <div class="w-3/4">
+            <div class="inline-block">
+                <div class="float-left">
+                    <a href="#" title=""><img src="imagens/mat-entrada.jpg" style="height: 87px"></a>
+                </div>
+                <div class="float-left">
+                    <a href="#" title="">
+                        <div class="ml-2 font-bold text-lg">Exame de Qualificação ao Mestrado 2020.2</div>
+                        <p class="ml-2">Inscrições para  Exame de Qualificação ao Mestrado 2020.2</p>
+                    </a>
+                    <div class="mt-2 float-right"><a href="#" title="">{{ __('Read more') }}</a></div>
+                </div>
+            </div>
+            <div class="inline-block">
+                <div class="float-left">
+                    <a href="#" title=""><img src="imagens/mat-entrada.jpg" style="height: 87px"></a>
+                </div>
+                <div class="float-left">
+                    <a href="#" title="">
+                        <div class="ml-2 font-bold text-lg">Exame de Qualificação ao Mestrado 2020.2</div>
+                        <p class="ml-2">Inscrições para  Exame de Qualificação ao Mestrado 2020.2</p>
+                    </a>
+                    <div class="mt-2 float-right"><a href="#" title="">{{ __('Read more') }}</a></div>
+                </div>
+            </div>
+            <div class="inline-block">
+                <div class="float-left">
+                    <a href="#" title=""><img src="imagens/mat-entrada.jpg" style="height: 87px"></a>
+                </div>
+                <div class="float-left">
+                    <a href="#" title="">
+                        <div class="ml-2 font-bold text-lg">Exame de Qualificação ao Mestrado 2020.2</div>
+                        <p class="ml-2">Inscrições para  Exame de Qualificação ao Mestrado 2020.2</p>
+                    </a>
+                    <div class="mt-2 float-right"><a href="#" title="">{{ __('Read more') }}</a></div>
+                </div>
+            </div>
+            <div class="inline-block">
+                <div class="float-left">
+                    <a href="#" title=""><img src="imagens/mat-entrada.jpg" style="height: 87px"></a>
+                </div>
+                <div class="float-left">
+                    <a href="#" title="">
+                        <div class="ml-2 font-bold text-lg">Exame de Qualificação ao Mestrado 2020.2</div>
+                        <p class="ml-2">Inscrições para  Exame de Qualificação ao Mestrado 2020.2</p>
+                    </a>
+                    <div class="mt-2 float-right"><a href="#" title="">{{ __('Read more') }}</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
