973317dec6bda7f9202031fbaaa9c0c7d4cb456e
José Antônio Freitas <jfreitas@unb.br>
2021-05-28 17:08:40 -0300

    Formulário de login.

--- resources/views/admin/gc.blade.php
+++ resources/views/admin/gc.blade.php
@@ -31,55 +31,84 @@
             </div>
         </header>
         <main>
-          <nav class="bg-gray-400">
-          <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div class="relative flex items-center justify-between h-16">
-              <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <!-- Mobile menu button-->
-                <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                  <span class="sr-only">Open main menu</span>
-                  
-                  <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
-              </div>
-              <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div class="hidden sm:block sm:ml-6">
-                  <div class="flex space-x-4">
-                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    <a href="{{ route('menus.site') }}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Menu Superior</a>
+            @auth
+                <nav class="bg-gray-400">
+                    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                        <div class="relative flex items-center justify-between h-16">
+                          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            <!-- Mobile menu button-->
+                            <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                              <span class="sr-only">Open main menu</span>
+                              
+                              <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                              </svg>
+                            </button>
+                          </div>
+                          <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                            <div class="hidden sm:block sm:ml-6">
+                              <div class="flex space-x-4">
+                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                                <a href="{{ route('menus.site') }}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Menu Superior</a>
 
-                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                                <a href="{{ route('conteudo.institucional') }}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Conteúdo Institucional</a>
 
-                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
-                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
-                  </div>
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+                            <!-- Profile dropdown -->
+                            <div class="ml-3 relative">
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <a class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" href="route('logout')" 
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                            {{ __('Log out') }}
+                                    </a>
+                                </form>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                </nav>
+                <div class="justify-center">
+                    @yield('menus_site')
+                    @yield('institucional')
                 </div>
-              </div>
-              <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            @endauth
 
-                <!-- Profile dropdown -->
-                <div class="ml-3 relative">
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-
-                        <a class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" href="route('logout')" 
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                                {{ __('Log out') }}
-                        </a>
-                    </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </nav>
-        <div class="justify-center">
-            @yield('menus_site')
-        </div>
+            @guest
+                @include('admin.login_gc')
+            @endguest
         </main>
         
 </body>
+
+<script src="{{ asset('node_modules/tinymce/tinymce.js') }}"></script>
+<script>
+    tinymce.init({
+        mode : "textareas",
+        // selector: 'textarea.institucinal_pt',
+        height: 300,
+
+        menubar: false,
+
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+        ],
+
+        toolbar: 'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+    });
+</script>
 </html>
